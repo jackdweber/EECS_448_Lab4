7674b7feb86e88e86284f845482539476230954b
Jack Weber <jackweber@ku.edu>
2017-09-25 10:50:08 -0500

    Created quiz.php. Loops through # of questions.

---
+++ quiz.php
@@ -0,0 +1,15 @@
+<?php
+    $correct_count = 0;
+
+    //List Questions
+    for($i = 1; $i <= 5; $i++){
+        echo $_POST["q".$i."_info"];
+        echo "<br /> Your Answer: ".$_POST["q".$i]."<br />";
+        echo "Correct Answer: ".$_POST["q".$i."_ans"];
+        echo "<hr>";
+
+        if($_POST["q".$i."_ans"] == $_POST["q".$i]) $correct_count++;
+    }
+
+    echo "Number of answers correct: ".$correct_count;
+?>
