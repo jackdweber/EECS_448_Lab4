9eb5f54c5924255564b4c3ff889410b7fa0962f1
Jack Weber <jackweber@ku.edu>
2017-09-25 12:39:42 -0500

    Created project directory.

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php include 'times_table.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lab 04 Directory</h1>
+    <ul>
+        <li><a href="times_table.php">Exercise 1: Times Table</a></li>
+        <li><a href="quiz.html">Exercise 2: Quiz</a></li>
+        <li><a href="customerFrontend.html">Exercise 3: Store</a></li>
+    </ul>
+</body>
+</html>
